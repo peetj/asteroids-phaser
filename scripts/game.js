b12bfd3b983c6316cae9c98b24d918122374f540
Pete Januarius <pete@nexgencodecamp.com.au>
2024-04-06 13:58:36 +1100

    Initial setup

---
+++ scripts/game.js
@@ -0,0 +1,31 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload () {
+    // Load assets here
+}
+
+function create () {
+    // Create game objects here
+}
+
+function update () {
+    // Game logic goes here
+}
