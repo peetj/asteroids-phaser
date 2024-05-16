911e96f6e7e947167f6f2d55f7ebc25d955dbc28
peetj <pete@nexgencodcamp.com.au>
2024-05-16 13:18:42 +1000

    Player movement and bullet

--- scripts/game.js
+++ scripts/game.js
@@ -7,7 +7,7 @@ var config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 300 },
+            gravity: { y: 0 },
             debug: false
         }
     },
@@ -22,6 +22,7 @@ var game = new Phaser.Game(config);
 function preload () {
     // Load assets here
     this.load.image('player', '/images/ship.png');
+    this.load.image('bullet', '/images/bullet.png');
 }
 
 function create () {
@@ -30,14 +31,42 @@ function create () {
 
     // Enable keyboard input
     this.cursors = this.input.keyboard.createCursorKeys();
+    key_spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    key_spacebar.on('down', ()=> {
+        fire.call(this)
+    });
+
+    this.bullets = this.physics.add.group();
 }
 
 function update () {
     // Update game information, object states...etc
     if(this.cursors.left.isDown){
-        player.angle -= 1;
+        player.angle -= 2;
     }
     else if(this.cursors.right.isDown){
-        player.angle += 1;
+        player.angle += 2;
     }
+
+}
+
+function fire() {
+    if(this.bullets.getChildren().length < 4) {
+        // Calculate the tip of the ship
+        const tipPosition = getTipPosition(player, 10); // adjust 30 to the correct distance from center to tip
+        // Create a bullet at the tip position
+        var bullet = this.bullets.create(tipPosition.x, tipPosition.y, 'bullet');
+        this.physics.velocityFromAngle(player.angle, 200, bullet.body.velocity); // adjust 200 to desired bullet speed
+    }
+
+
+}
+
+
+function getTipPosition(ship, distanceFromCenter) {
+    const radians = Phaser.Math.DegToRad(ship.angle);
+    return {
+        x: ship.x + distanceFromCenter * Math.cos(radians),
+        y: ship.y + distanceFromCenter * Math.sin(radians)
+    };
 }
